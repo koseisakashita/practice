508068bac012067e264a5cd243952cfe7b6f78b3
koseisakashita <s09072302394@gmail.com>
2018-05-12 15:13:02 +0900

    ベース環境の設定をした。

---
+++ cakephp/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp   = require('gulp')
+var coffee = require('gulp-coffee')
+var sass = require('gulp-sass')
+
+gulp.task ('compile-coffee', function(){
+  gulp.src('./assets/coffee/**/*.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('./webroot/js/'))
+})
+
+
+
+gulp.task('sass', function(){
+    //scssディレクトリの指定
+    gulp.src('./assets/sass/**/*.scss')
+    //コンパイル実行
+    .pipe(sass({style : 'expanded'})) //出力形式の種類　#nested, compact, compressed, expanded.
+    //出力先の指定
+    .pipe(gulp.dest('./webroot/css/'));
+});
+
+
+//自動監視のタスクを作成(watchと名付ける)
+gulp.task('watch', ['sass'], function(){
+  var watcher = gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  watcher.on('change', function(event) {
+  });
+});
+
+gulp.task('default', ['watch','compile-coffee']);
